22d4a38e931ade73449d75ad23b60b4a8ad0333c
bjoernnnn <dreselbjoern@gmail.com>
2026-06-15 22:15:21 +0200

    Initial Commit

---
+++ KletterTagebuch/KletterTagebuchApp.swift
@@ -0,0 +1,17 @@
+//
+//  KletterTagebuchApp.swift
+//  KletterTagebuch
+//
+//  Created by Björn Dresel on 2026-06-15.
+//
+
+import SwiftUI
+
+@main
+struct KletterTagebuchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
